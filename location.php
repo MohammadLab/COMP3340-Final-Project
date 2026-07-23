7b48f3cba09d854f25b98ad50ba84bdec6d725c5
MohammadLab <moe.labak@gmail.com>
2026-07-23 19:21:41 -0400

    update location address

--- location.php
+++ location.php
@@ -4,10 +4,10 @@
 <main>
     <h2>Our Location</h2>
 
-    <p>Moe's PC Parts is located at 123 Tech Street in Windsor, Ontario.</p>
+    <p>Moe's PC Parts<br>401 Sunset Avenue<br>Windsor, ON N9B 3P4</p>
 
     <iframe
-        src="https://www.google.com/maps?q=Windsor,%20Ontario&output=embed"
+        src="https://www.google.com/maps?q=401%20Sunset%20Avenue,%20Windsor,%20Ontario&output=embed"
         width="600"
         height="450"
         style="border:0;"
